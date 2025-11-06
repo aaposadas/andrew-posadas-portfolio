7b942d28c4d482bae709ee96815918c10520f701
Andrew Posadas <andrewposadas5@gmail.com>
2025-11-06 15:39:01 -0600

    vercel-insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Montserrat, Roboto } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
